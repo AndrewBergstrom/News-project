25f453038289c5a6d6ed8d64a636431037b5ef20
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2020-06-22 09:16:50 -1000

    ajax added in script

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 var APIkey = "vEBaIAK7ouqfdNIA6kemhIDx6XKGMO27"
 
-var newsURL
+var newsURL =`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=${APIkey}`
+
+$.ajax({
+    url: newsURL,
+    method:"GET"
+}) .then(function(response){
+    console.log(response)
+})
+
